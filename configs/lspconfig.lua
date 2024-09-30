7d1a7a32c41c125f33ca05bb497fe4d02343748d
Kai Sawamoto <kaisawamoto@gmail.com>
2024-09-29 17:17:50 -0700

    add flutter

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -6,7 +6,7 @@ local util = require "lspconfig/util"
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "clangd" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "dartls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- plugins.lua
+++ plugins.lua
@@ -206,6 +206,17 @@ local plugins = {
     },
   },
 
+  -- Flutter
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+
   -- Rust
 
   {
